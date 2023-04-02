7792bfd4fd09e2d8c737ad9e1549a52d707dd02c
MariaBanaszkiewicz <xmox@interia.pl>
2023-04-02 16:15:55 +0200

    lab3 assigment 1-3&5-8

--- lab3/Controllers/MoviesController.cs
+++ lab3/Controllers/MoviesController.cs
@@ -1,19 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 namespace lab3.Controllers;
 [ApiController]
 [Route("[controller]")]
-public class MoviesController: ControllerBase {
+public class MoviesController : ControllerBase
+{
     [HttpPost("UploadMovieCsv")]
-    public string Post(IFormFile inputFile) {
+    public string Post(IFormFile inputFile)
+    {
         var strm = inputFile.OpenReadStream();
         byte[] buffer = new byte[inputFile.Length];
-        strm.Read(buffer, 0, (int) inputFile.Length);
+        strm.Read(buffer, 0, (int)inputFile.Length);
         string fileContent = System.Text.Encoding.Default.GetString(buffer);
         strm.Close();
         MoviesContext dbContext = new MoviesContext();
         bool skip_header = true;
-        foreach(string line in fileContent.Split('\n')) {
-            if (skip_header) {
+        foreach (string line in fileContent.Split('\n'))
+        {
+            if (skip_header)
+            {
                 skip_header = false;
                 continue;
             }
@@ -22,15 +27,17 @@ public class MoviesController: ControllerBase {
             string MovieID = tokens[0];
             string MovieName = tokens[1];
             string[] Genres = tokens[2].Split("|");
-            List < Genre > movieGenres = new List < Genre > ();
-            foreach(string genre in Genres) {
+            List<Genre> movieGenres = new List<Genre>();
+            foreach (string genre in Genres)
+            {
                 Genre g = new Genre();
                 g.Name = genre;
-                if (!dbContext.Genres.Any(e => e.Name == g.Name)) {
+                if (!dbContext.Genres.Any(e => e.Name == g.Name))
+                {
                     dbContext.Genres.Add(g);
                     dbContext.SaveChanges();
                 }
-                IQueryable < Genre > results = dbContext.Genres.Where(e => e.Name == g.Name);
+                IQueryable<Genre> results = dbContext.Genres.Where(e => e.Name == g.Name);
                 if (results.Count() > 0)
                     movieGenres.Add(results.First());
             }
@@ -46,19 +53,22 @@ public class MoviesController: ControllerBase {
     }
 
     [HttpGet("GetAllGenres")]
-    public IEnumerable < Genre > GetAllGenres() {
+    public IEnumerable<Genre> GetAllGenres()
+    {
         MoviesContext dbContext = new MoviesContext();
         return dbContext.Genres.AsEnumerable();
     }
 
     [HttpGet("GetMoviesByName/{search_phrase}")]
-    public IEnumerable < Movie > GetMoviesByName(string search_phrase) {
+    public IEnumerable<Movie> GetMoviesByName(string search_phrase)
+    {
         MoviesContext dbContext = new MoviesContext();
         return dbContext.Movies.Where(e => e.Title.Contains(search_phrase));
     }
 
     [HttpPost("GetMoviesByGenre")]
-    public IEnumerable < Movie > GetMoviesByGenre(string search_phrase) {
+    public IEnumerable<Movie> GetMoviesByGenre(string search_phrase)
+    {
         MoviesContext dbContext = new MoviesContext();
         return dbContext.Movies.Where(
             m => m.Genres.Any(p => p.Name.Contains(search_phrase))
@@ -66,9 +76,103 @@ public class MoviesController: ControllerBase {
     }
 
     [HttpGet("GetGenresByMovie/{movieId}")]
-    public IEnumerable<Genre> GetGenresByMovie(int movieId) {
+    public IEnumerable<Genre> GetGenresByMovie(int movieId)
+    {
         MoviesContext dbContext = new MoviesContext();
-        return dbContext.Genres.Where(e => e.Movies.Any( e=>e.MovieID==movieId ));
+        return dbContext.Genres.Where(e => e.Movies.Any(e => e.MovieID == movieId));
+    }
+
+    [HttpGet("GetGenresVectorByMovie/{movieId}")]
+    public List<int> GetGenresVectorByMovie(int movieId)
+    {
+        MoviesContext dbContext = new MoviesContext();
+        IEnumerable<Movie> movies = dbContext.Movies.Include(movie => movie.Genres).Where(m => m.MovieID == movieId);
+        if (movies.Count() < 1)
+        {
+            throw new ArgumentException("invalid movie id");
+        }
+        Movie movie = movies.First();
+        List<int> vector = new List<int>();
+        foreach (Genre genre in dbContext.Genres)
+        {
+            if (movie.Genres.Any(g => g.GenreID == genre.GenreID))
+            {
+                vector.Add(1);
+            }
+            else
+            {
+                vector.Add(0);
+            }
+
+        }
+        return vector;
+    }
+
+    [HttpGet("GetCosineSimilarity/{movieId1}/{movieId2}")]
+    public double GetCosineSimilarity(int movieId1, int movieId2)
+    {
+
+        List<int> vec1 = GetGenresVectorByMovie(movieId1);
+        List<int> vec2 = GetGenresVectorByMovie(movieId2);
+
+        int m1m2 = 0;
+        int m12 = 0;
+        int m22 = 0;
+
+        for (int i = 0; i < vec1.Count(); i++)
+        {
+            m1m2 += vec1[i] * vec2[i];
+            m12 += vec1[i] * vec1[i];
+            m22 += vec2[i] * vec2[i];
+        }
+
+        return (m1m2) / (Math.Sqrt(m12) * Math.Sqrt(m22));
+    }
+
+    //TODO: niedziała :((
+    /*[HttpGet("GetMoviesSharingGenres/{movieId}")]
+    public IEnumerable<Movie> GetMoviesSharingGenres(int movieId)
+    {
+        MoviesContext dbContext = new MoviesContext();
+
+        IEnumerable<Movie> moviesWithId = dbContext.Movies.Include(movie => movie.Genres).Where(m => m.MovieID == movieId);
+        if (moviesWithId.Count() < 1)
+        {
+            throw new ArgumentException("invalid movie id");
+        }
+        Movie movie = moviesWithId.First();
+        //IEnumerable<Movie> resultMovies = dbContext.Movies.Where(m => m.Genres.Any(g=>movie.Genres.Any(genre => g.GenreID == genre.GenreID)));
+        return moviesWithId;
+    }*/
+
+    [HttpGet("GetSimilarMovies/{movieId}/{treshhold}")]
+    public IEnumerable<Movie> GetSimilarMovies(int movieId, double treshhold)
+    {
+        MoviesContext dbContext = new MoviesContext();
+        List<Movie> resultMovies = new List<Movie>();
+
+        foreach (Movie movie in dbContext.Movies)
+        {
+            double sim = GetCosineSimilarity(movieId, movie.MovieID);
+            if (sim >= treshhold)
+            {
+                resultMovies.Add(movie);
+            }
+        }
+        return resultMovies;
+    }
+
+    [HttpGet("GetSimilarMoviesToTopOne/{userId}")]
+    public IEnumerable<Movie> GetSimilarMoviesToTopOne(int userId){
+
+        MoviesContext dbContext = new MoviesContext();
+        IEnumerable<Rating> usersRatings = dbContext.Ratings.Include(r => r.RatedMovie).Where(
+            r => r.RatingUser.UserID == userId
+        );
+        Movie topMovie = usersRatings.OrderByDescending(r=>r.RatingValue).First().RatedMovie;
+        
+        return GetSimilarMovies(topMovie.MovieID, 0.9);
+
     }
 
 }
--- lab3/Controllers/RatingsController.cs
+++ lab3/Controllers/RatingsController.cs
@@ -1,84 +1,82 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using System.Globalization;
 namespace lab3.Controllers;
 [ApiController]
 [Route("[controller]")]
-public class RatingsController: ControllerBase {
+public class RatingsController : ControllerBase
+{
 
-    [HttpPost("UploadRatingsCsv")]
-    public string PostRatings(IFormFile inputFile) {
+    [HttpPost("UploadRatingCsv")]
+    public string Post(IFormFile inputFile)
+    {
         var strm = inputFile.OpenReadStream();
         byte[] buffer = new byte[inputFile.Length];
-        strm.Read(buffer, 0, (int) inputFile.Length);
+        strm.Read(buffer, 0, (int)inputFile.Length);
         string fileContent = System.Text.Encoding.Default.GetString(buffer);
         strm.Close();
         MoviesContext dbContext = new MoviesContext();
         bool skip_header = true;
-        foreach(string line in fileContent.Split('\n')) {
-            if (skip_header) {
+        int i = 0;
+        foreach (string line in fileContent.Split('\n'))
+        {
+            if (skip_header)
+            {
                 skip_header = false;
                 continue;
             }
+            i++;
             var tokens = line.Split(",");
-            if (tokens.Length < 2) continue;
+            if (tokens.Length != 4) continue;
+            string UserID = tokens[0];
+            string MovieID = tokens[1];
             string RatingValue = tokens[2];
-            string RatedMovieID = tokens[1];
-            string RatingUserId = tokens[0];
+            string timestamp = tokens[3];
+            User u = new User();
+            u.UserID = int.Parse(UserID);
+            if (!dbContext.Users.Any(e => e.UserID == u.UserID))
+            {
+                u.Name = UserID;
+                dbContext.Users.Add(u);
+                dbContext.SaveChanges();
+            }
+            IQueryable<User> userResults = dbContext.Users.Where(e => e.UserID == u.UserID);
+            Movie m = new Movie();
+            m.MovieID = int.Parse(MovieID);
+            if (!dbContext.Movies.Any(e => e.MovieID == m.MovieID))
+            {
+                m.Title = MovieID;
+                dbContext.Movies.Add(m);
+                dbContext.SaveChanges();
+            }
+            IQueryable<Movie> movieResults = dbContext.Movies.Where(e => e.MovieID == m.MovieID);
 
             Rating r = new Rating();
-            int res=0;
-            r.RatingValue=float.Parse(RatingValue, CultureInfo.InvariantCulture.NumberFormat);
-            int.TryParse(RatingValue,out res);
+            r.RatingUser = userResults.First();
+            r.RatedMovie = movieResults.First();
+            r.RatingValue = int.Parse(RatingValue.Split('.')[0]);
+            r.RatingID = i;
 
-            IQueryable < Movie > movieResult = dbContext.Movies.Where(e => e.MovieID == int.Parse(RatedMovieID));
-            System.Console.Write(movieResult.Count());
-            System.Console.Write("\n");
-            
-            if(movieResult.Count() ==0) {
-                continue;
-            }
-            System.Console.Write(movieResult.First().MovieID);
-            System.Console.Write("\n");
-            r.RatedMovie = movieResult.First();
-
-            User user = new User();
-            user.UserID = int.Parse(RatingUserId);
-            user.Name="";
-            IQueryable < User > userResults = dbContext.Users.Where(e => e.UserID == user.UserID);
-                if (userResults.Count() > 0) {
-                    user = userResults.First();
-                }
-                else {
-                    dbContext.Users.Add(user);
-                    dbContext.SaveChanges();
-                }
-            r.RatingUser = user;
-            user=new User();
-
-            dbContext.Ratings.Add(r);
-
+            if (!dbContext.Ratings.Any(e => e.RatingID == r.RatingID)) dbContext.Ratings.Add(r);
         }
         dbContext.SaveChanges();
         return "OK";
     }
 
-//w sortedRatingsList Mvie i User są zawsze null...
-    [HttpGet("GetUserRecommendations/{userId}")]
-    public IEnumerable<Movie> GetUserRecommendations(int userId) {
-        System.Console.Write("wchodzę");
+    [HttpGet("GetUserRatings/{userId}")]
+    public IEnumerable<Rating> GetUserRatings(int userId)
+    {
         MoviesContext dbContext = new MoviesContext();
-        IEnumerable <Rating> usersRatings = dbContext.Ratings.Where(
+        IEnumerable<Rating> usersRatings = dbContext.Ratings.Include(r => r.RatedMovie).Where(
             r => r.RatingUser.UserID == userId
         );
-        List < Movie > ratedMovies = new List < Movie > ();
-        usersRatings.ToList();
-        List<Rating> SortedRatingsList = usersRatings.ToList().OrderBy(o=>o.RatingValue).ToList();
-        foreach(Rating rating in SortedRatingsList){
-            System.Console.Write(rating.RatingID);
-        }
-            
-        
-        return ratedMovies; 
-        }
+        return usersRatings;
+    }
+
+    [HttpGet("GetSortedUserRatings/{userId}")]
+    public IEnumerable<Rating> GetSortedUserRatings(int userId)
+    {
+
+        return GetUserRatings(userId).OrderByDescending(r => r.RatingValue);
+    }
 }
 
